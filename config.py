9b40a9f704d82d8679ac57db13bfabc2439a5e73
lilovy <iv1hanz@gmail.com>
2023-07-10 02:13:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ JWT_ALGORITHM = "HS256"
 
 BASE_API_PATH = "/api/v1"
 
-db_path = "src\database\multigpt.db"
+db_path = "postgres://manygpt_user:Op5FSJ7zgBB450mewzKNr1op5RsHJKHs@dpg-cili2r95rnuvtgso8s50-a/manygpt"
 
 token = "o6BvUBrXYXuis5Xb5Y1CLg%3D%3D"
 
